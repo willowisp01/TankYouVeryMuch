1516a99780a080aa9fc02c533ec814fe5cbf75ff
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2023-05-22 21:50:04 +0800

    fixed rotation bug

--- TYVM Game/Assets/Scripts/Player/PlayerMovement.cs
+++ TYVM Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,20 +8,22 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 10f;
-    private Rigidbody2D myBody;
+  
     public Camera cam;
-    private Vector2 movement;
-    public Vector2 mousePos;
-
-    public float angle;
-
-    public Vector2 lookDir;
-
-
+    private Vector2 movement, mousePos;
+    private float angle;
+    private Vector2 lookDir;
+    private GameObject tankMain, tankTower;
+    private Rigidbody2D tankTowerBody, tankMainBody;
 
     private void Awake() {
-        myBody = GetComponent<Rigidbody2D>();
         //this can also be done via unity drag and drop. 
+        tankMain = transform.Find("SmallTankA").gameObject; 
+        tankTower = transform.Find("Tower").gameObject;
+        tankMainBody = tankMain.GetComponent<Rigidbody2D>();
+        tankTowerBody = tankTower.GetComponent<Rigidbody2D>();
+
+        //can be replaced with tags possibly to avoid hardcoding the name... https://answers.unity.com/questions/893966/how-to-find-child-with-tag.html
     }
 
     // Start is called before the first frame update
@@ -38,16 +40,15 @@ public class PlayerMovement : MonoBehaviour
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition); //mouse position (in world coordinates)
     }
 
-    private void FixedUpdate() {
-        myBody.MovePosition(myBody.position + movement * moveSpeed * Time.deltaTime);
-        lookDir = mousePos - myBody.position; //vector subtraction
+    private void FixedUpdate() { //for physics stuff
+        Vector2 newPosition = tankMainBody.position + movement * moveSpeed * Time.deltaTime;
+        tankMainBody.MovePosition(newPosition);
+        tankTowerBody.MovePosition(newPosition);
+        lookDir = mousePos - tankMainBody.position; //vector subtraction
         angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90; //arctangent. we minus 90 because of the 4 quadrants trigonometry.
-        myBody.rotation = angle;    
+        tankTowerBody.rotation = angle;    
     }
 
-    void PlayerMove() {
-        
-
-        //TODO: https://www.youtube.com/watch?v=LNLVOjbrQj4 implement the shooting and rotation in a neater way
-    }
+    //TODO: https://www.youtube.com/watch?v=LNLVOjbrQj4 implement the shooting and rotation in a neater way
+    
 }
