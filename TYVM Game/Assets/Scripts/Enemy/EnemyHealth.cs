cd7d54183b42571b6f168ebc273a3e82fa9ef19c
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2023-06-05 23:06:06 +0800

    Enemy can now shoot at player. Both enemy and player takes damage

--- TYVM Game/Assets/Scripts/Enemy/EnemyHealth.cs
+++ TYVM Game/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+private int health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        setHealth(3);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            destroySelf();
+        }
+    }
+
+    public void destroySelf() {
+        Debug.Log("enemy died");
+        //TODO: put some explosion effect or something
+        Destroy(gameObject);
     }
 }
--- TYVM Game/Assets/Scripts/Projectiles/ProjectileBehaviour.cs
+++ TYVM Game/Assets/Scripts/Projectiles/ProjectileBehaviour.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class ProjectileBehaviour : MonoBehaviour {
 
     [SerializeField]
+
+    private int damage = 1; //how much damage the bullet does (default of 1)
+    private int durability = 10; //bullet durability (such that we don't reflect a ridiculous number of times).
+    //(feel free to change the above)
+
     private float bulletDuration = 3f;
     private Rigidbody2D rb;
     private Vector2 oldVelocity;
@@ -36,10 +41,41 @@ public class ProjectileBehaviour : MonoBehaviour {
     // Behaviour for the projectile upon collision with different game objects
     private void OnCollisionEnter2D(Collision2D collision) {
         // The projectile reflects upon collision with a wall
+
+        //GameObject other = collision.collider.gameObject; //could be a wall, tank or wtv
+        //GameObject bullet = collision.otherCollider.gameObject; //iis always the bullet
+        //Debug.Log("Other: " + other.gameObject);
+        //Debug.Log("Bullet: " + bullet.gameObject);
+
+
         if (collision.collider.CompareTag("Wall")) {
+            durability -= 1;
             rb.velocity = Vector2.Reflect(oldVelocity, collision.GetContact(0).normal); // collision.GetContact(0).normal gets the unit vector perpendicular to the object
             rb.transform.up = rb.velocity; // The projectile now faces the new direction
+            if (durability <= 0) {
+                DestroyProjectile(); //unfortunately this cuts the sound short. many possible ways to fix it
+            }
         }
+
+        if (collision.collider.CompareTag("PlayerHull")) { //note this is playerhull and NOT player!
+            //GameObject playerTank = collision.collider.gameObject.transform.parent.gameObject;
+            //Debug.Log(playerTank);
+            Debug.Log("Player was hit!");
+            Health playerHealth = collision.collider.GetComponentInParent<Health>();
+            playerHealth.takeDamage(damage);
+            DestroyProjectile();
+        } else if (collision.collider.CompareTag("EnemyHull")) {
+            //GameObject enemyTank = collision.collider.gameObject;
+            //Debug.Log(enemyTank);
+            Debug.Log("Enemy was hit!");
+            EnemyHealth enemyHealth = collision.collider.GetComponentInParent<EnemyHealth>();
+            enemyHealth.takeDamage(damage);
+            DestroyProjectile();
+        }
+
+        //Debug.Log("Player Health: " + playerHealth);
+        //Debug.Log("Enemy Health: " + enemyHealth);
+
         // Look into another method (a dictionary? switch?) to assign different behaviours to different colliders, otherwise we'll have a million if statements
     }
 }
