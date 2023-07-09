216a16e37c899d49fba8d63c333ea2b370d806ac
Ronn <ronnng1510@gmail.com>
2023-07-09 19:37:30 +0800

    gameevent and gameeventlistener

---
+++ TYVM Game/Assets/Scripts/GameManagement/Events/GameEventListener.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour {
+
+    [SerializeField]
+    private GameEvent gameEvent; // The event associated with this listener
+
+    [SerializeField]
+    private UnityEvent nextEvent; // The subsequent event to be triggered when the game event triggers
+
+    private void OnEnable() {
+        gameEvent.AddListener(this);
+    }
+
+    private void OnDisable() {
+        gameEvent.RemoveListener(this);
+    }
+
+    public void OnEventTriggered() {
+        nextEvent.Invoke();
+    }
+}
