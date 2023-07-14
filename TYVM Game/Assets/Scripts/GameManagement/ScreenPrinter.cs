d2be5984a4ba2450c0e3bf7a3ee9dd3f852075fc
Ronn <ronnng1510@gmail.com>
2023-07-14 20:25:53 +0800

    Update ScreenPrinter.cs

--- TYVM Game/Assets/Scripts/GameManagement/ScreenPrinter.cs
+++ TYVM Game/Assets/Scripts/GameManagement/ScreenPrinter.cs
@@ -22,12 +22,12 @@ public class ScreenPrinter : MonoBehaviour {
     [SerializeField]
     private GameEventListener defeatListener;
 
-    private int stageNumber;
+    private string stage;
 
     private void Awake() {
         victoryListener.nextEvent.AddListener(() => Result("VICTORY"));
         defeatListener.nextEvent.AddListener(() => Result("DEFEAT"));
-        stageNumber = SceneManager.GetActiveScene().buildIndex;
+        stage = SceneManager.GetActiveScene().name;
     }
 
     private void Result(string res) {
@@ -37,11 +37,11 @@ public class ScreenPrinter : MonoBehaviour {
         switch (res) {
             case "VICTORY":
                 message.text = "All enemies killed!";
-                summary.text = "Stage " + stageNumber + " cleared!\n" + summary.text;
+                summary.text = stage + " cleared!\n" + summary.text;
                 break;
             case "DEFEAT":
                 message.text = "You died...";
-                summary.text = "Stage " + stageNumber + " failed...\n" + summary.text;
+                summary.text = stage + " failed...\n" + summary.text;
                 break;
         }
     }
